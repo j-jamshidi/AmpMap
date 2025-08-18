f3b543af61ad67af5f8ca7ec5e3638aae6633b66
j-jamshidi <javadjamshidi@gmail.com>
2025-08-18 13:45:10 +1000

    all docker V3.0.1

--- script/run.sh
+++ script/run.sh
@@ -1,26 +1,14 @@
 #!/bin/bash
 
-# Script: run.sh
-# Description: Pipeline for processing and phasing ONT amplicon data.
-# Works for phasing when two variants are provided, or QC when one or none is provided.
-#===============================================================================
-# Configuration and Setup
-#===============================================================================
+# ONT Amplicon Analysis Pipeline - Containerized Version
+# Processes barcoded amplicon sequences for variant calling and phasing
 
 RUNID=$1
 BASEDIR="/EBSDataDrive/ONT/Runs/${RUNID}"
 WORKDIR="${BASEDIR}/result"
-
-# Reference and tool paths
-#Clair3 docker image. docker pull hkubal/clair3:latest
-#HapCUT2 docker image. docker pull javadj/hapcut2:latest
-#WhatsHap docker image. docker pull javadj/whatshap:latest
 REFERENCE="/EFSGaiaDataDrive/ref/ONT/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna"
-SCRIPT_PATH="/EBSDataDrive/ONT/script"
 
-#===============================================================================
 # Functions
-#===============================================================================
 
 # Logging function
 log() {
@@ -74,10 +62,8 @@ merge_bam_files() {
     local coordinate=$3
 
     log "Merging BAM files for ${barcode}, sample ${episode}..."
-    docker run --rm -v "${BASEDIR}:/input" -v "${WORKDIR}:/output" --entrypoint samtools javadj/ontampip:latest \
-        merge -@ 6 -u - /input/bam_pass/${barcode}/*bam | \
-    docker run --rm -i -v "${WORKDIR}:/output" --entrypoint samtools javadj/ontampip:latest \
-        sort -@ 6 -o /output/${barcode}/temp.bam && \
+    docker run --rm -v "${BASEDIR}:/input" -v "${WORKDIR}:/output" --entrypoint sh javadj/ontampip:latest \
+        -c "samtools merge -@ 6 -u - /input/bam_pass/${barcode}/*.bam | samtools sort -@ 6 -o /output/${barcode}/temp.bam" && \
     docker run --rm -v "${WORKDIR}:/output" --entrypoint samtools javadj/ontampip:latest \
         index /output/${barcode}/temp.bam && \
     docker run --rm -v "${WORKDIR}:/output" --entrypoint samtools javadj/ontampip:latest \
@@ -116,7 +102,6 @@ run_clair3() {
         --min_coverage=20 >/dev/null 2>&1
             log "Clair3 analysis finished!"
 }
-
 
 # Run WhatsHap for phasing
 run_whatshap() {
@@ -248,16 +233,16 @@ generate_xml_single() {
     OUTXML="${WORKDIR}/${Barcode}/${Episode}.xml"
     
     if [ "$EpisodeWES" == "NA" ]; then
-        docker run --rm -v "${WORKDIR}:/data" --entrypoint cp javadj/ontamp:latest /app/solo_LR.xml "/data/${Barcode}/${Episode}.xml"
+        docker run --rm -v "${WORKDIR}:/data" --entrypoint cp javadj/ontampip:latest /app/solo_LR.xml "/data/${Barcode}/${Episode}.xml"
     else
         cat /EBSDataDrive/software/sample_ran.txt /EBSDataDrive/software/sample_ran_CRE_BS.txt > /EBSDataDrive/software/sample_SR.txt
         wesrun=$(cat /EBSDataDrive/software/sample_SR.txt | grep $EpisodeWES | cut -f 6 | tr '[:lower:]' '[:upper:]')
         sid=$(cat /EBSDataDrive/software/sample_SR.txt | grep $EpisodeWES | cut -f 1 | tr '[:lower:]' '[:upper:]')
         
         if [ -z "$wesrun" ]; then
-            docker run --rm -v "${WORKDIR}:/data" --entrypoint cp javadj/ontamp:latest /app/solo_LR.xml "/data/${Barcode}/${Episode}.xml"
+            docker run --rm -v "${WORKDIR}:/data" --entrypoint cp javadj/ontampip:latest /app/solo_LR.xml "/data/${Barcode}/${Episode}.xml"
         else
-            docker run --rm -v "${WORKDIR}:/data" --entrypoint cp javadj/ontamp:latest /app/solo_LR_SR.xml "/data/${Barcode}/${Episode}.xml"
+            docker run --rm -v "${WORKDIR}:/data" --entrypoint cp javadj/ontampip:latest /app/solo_LR_SR.xml "/data/${Barcode}/${Episode}.xml"
             read -r BAMpresign BAIpresign <<< $(get_presign)
             bamurl=$(echo $BAMpresign | sed -r 's/\//\\\//g' | sed -r 's/&/\\&amp;/g')
             baiurl=$(echo $BAIpresign | sed -r 's/\//\\\//g' | sed -r 's/&/\\&amp;/g')
@@ -313,113 +298,6 @@ generate_xml_single() {
     sed -i -e "s/C1_LR_index_URL/$lrbai/g" "${OUTXML}"
     
     log "XML file created: ${OUTXML}"
-}
-
-# Generate XML files (old function - kept for reference)
-generate_xml() {
-    local run=$1
-    log "XML generation function called with run: $run"
-    log "Reading from file: ${BASEDIR}/${run}.info"
-    
-    if [[ ! -f "${BASEDIR}/${run}.info" ]]; then
-        log "Error: Input file ${BASEDIR}/${run}.info does not exist!"
-        return 1
-    fi
-    
-    log "Input file exists, proceeding with XML generation..."
-    
-    # get_presign function
-    get_presign() {
-        DATID=$(bs -c POWH list datasets --input-biosample $sid --not-type "illumina.fastq.v1.8" --sort-by AppSession.DateCreated --terse | tail -n1)
-        BAM=$(bs dataset -c POWH content --id=$DATID --extension=bam --terse)
-        BAI=$(bs dataset -c POWH content --id=$DATID --extension=bam.bai --terse)
-        
-        AWS_RSA256_link=$(bs -c POWH file link -i "$BAM")
-        wget_out=$(wget --save-headers --max-redirect=0 -O - "$AWS_RSA256_link" 2>&1)
-        BAMPre=$(echo "$wget_out" | grep -i "Location" | tail -n 1 | awk '{print $2}')
-        
-        AWS_RSA256_link=$(bs -c POWH file link -i "$BAI")
-        wget_out=$(wget --save-headers --max-redirect=0 -O - "$AWS_RSA256_link" 2>&1)
-        BAIPre=$(echo "$wget_out" | grep -i "Location" | tail -n 1 | awk '{print $2}')
-        
-        echo $BAMPre $BAIPre
-    }
-    
-    while IFS=, read -r Batch Barcode Episode Coordinate Variant1 Variant2 EpisodeWES remainder; do
-        # Clean up variables
-        Barcode=$(echo "$Barcode" | tr -d ' ')
-        Episode=$(echo "$Episode" | tr -d ' ')
-        EpisodeWES=$(echo "$EpisodeWES" | tr -d ' ')
-        
-        OUTXML="${WORKDIR}/${Barcode}/${Episode}.xml"
-        log "Generating XML for ${Barcode}/${Episode}..."
-        
-        if [ "$EpisodeWES" == "NA" ]; then
-            docker run --rm -v "${WORKDIR}:/data" --entrypoint cp javadj/ontampip:latest /app/solo_LR.xml "/data/${Barcode}/${Episode}.xml"
-        else
-            cat /EBSDataDrive/software/sample_ran.txt /EBSDataDrive/software/sample_ran_CRE_BS.txt > /EBSDataDrive/software/sample_SR.txt
-            wesrun=$(cat /EBSDataDrive/software/sample_SR.txt | grep $EpisodeWES | cut -f 6 | tr '[:lower:]' '[:upper:]')
-            sid=$(cat /EBSDataDrive/software/sample_SR.txt | grep $EpisodeWES | cut -f 1 | tr '[:lower:]' '[:upper:]')
-            
-            if [ -z "$wesrun" ]; then
-                docker run --rm -v "${WORKDIR}:/data" --entrypoint cp javadj/ontampip:latest /app/solo_LR.xml "/data/${Barcode}/${Episode}.xml"
-            else
-                docker run --rm -v "${WORKDIR}:/data" --entrypoint cp javadj/ontampip:latest /app/solo_LR_SR.xml "/data/${Barcode}/${Episode}.xml"
-                read -r BAMpresign BAIpresign <<< $(get_presign)
-                bamurl=$(echo $BAMpresign | sed -r 's/\//\\\//g' | sed -r 's/&/\\&amp;/g')
-                baiurl=$(echo $BAIpresign | sed -r 's/\//\\\//g' | sed -r 's/&/\\&amp;/g')
-                sed -i -e "s/C1_SR_index_URL/$baiurl/g" "${OUTXML}"
-                sed -i -e "s/C1_SR_BAM_URL/$bamurl/g" "${OUTXML}"
-            fi
-        fi
-        
-        sed -i -e "s/C1_IID/$Episode/g" "${OUTXML}"
-        raw2=$Variant2
-        
-        if [[ "$Variant1" =~ chr ]] || [[ "$Variant2" =~ chr ]]; then
-            if [[ ! "$Variant2" =~ chr ]]; then
-                Variant2=$Variant1
-            fi
-            chr1=$(echo $Variant1 | cut -d ':' -f 1)
-            pos1=$(echo $Variant1 | cut -d ':' -f 2 | cut -d ' ' -f 1)
-            chr2=$(echo $Variant2 | cut -d ':' -f 1)
-            pos2=$(echo $Variant2 | cut -d ':' -f 2 | cut -d ' ' -f 1)
-            
-            if [ "$chr1" != "$chr2" ]; then
-                log "Warning: Variants are on different chromosomes for ${Episode}. Skipping XML generation."
-                continue
-            fi
-            
-            if [ $pos1 -lt $pos2 ]; then
-                start=$((pos1 - 100))
-                end=$((pos2 + 100))
-            else
-                start=$((pos2 - 100))
-                end=$((pos1 + 100))
-            fi
-            Coordinate="${chr1}:${start}-${end}"
-        fi
-        
-        sed -i -e "s/CHRSTARTEND/$Coordinate/g" "${OUTXML}"
-        Variant2=$raw2
-        
-        if [[ "$Variant2" =~ chr ]]; then
-            lrbam=$(aws s3 presign s3://nswhp-gaia-poc-pl/ONT/$run/$Barcode/${Episode}_phased.bam --expire=604800 | sed -r 's/\//\\\//g' | sed -r 's/&/\\&amp;/g')
-            lrbai=$(aws s3 presign s3://nswhp-gaia-poc-pl/ONT/$run/$Barcode/${Episode}_phased.bam.bai --expire=604800 | sed -r 's/\//\\\//g' | sed -r 's/&/\\&amp;/g')
-            phasevcf=$(aws s3 presign s3://nswhp-gaia-poc-pl/ONT/$run/$Barcode/${Episode}_Phased.vcf.gz --expire=604800 | sed -r 's/\//\\\//g' | sed -r 's/&/\\&amp;/g')
-            wfvcf=$(aws s3 presign s3://nswhp-gaia-poc-pl/ONT/$run/$Barcode/${Episode}.wf_snp.vcf.gz --expire=604800 | sed -r 's/\//\\\//g' | sed -r 's/&/\\&amp;/g')
-        else
-            lrbam=$(aws s3 presign s3://nswhp-gaia-poc-pl/ONT/$run/$Barcode/${Episode}_QC.bam --expire=604800 | sed -r 's/\//\\\//g' | sed -r 's/&/\\&amp;/g')
-            lrbai=$(aws s3 presign s3://nswhp-gaia-poc-pl/ONT/$run/$Barcode/${Episode}_QC.bam.bai --expire=604800 | sed -r 's/\//\\\//g' | sed -r 's/&/\\&amp;/g')
-            phasevcf=$(aws s3 presign s3://nswhp-gaia-poc-pl/ONT/$run/$Barcode/${Episode}.vcf --expire=604800 | sed -r 's/\//\\\//g' | sed -r 's/&/\\&amp;/g')
-            wfvcf=$(aws s3 presign s3://nswhp-gaia-poc-pl/ONT/$run/$Barcode/${Episode}.wf_snp.vcf.gz --expire=604800 | sed -r 's/\//\\\//g' | sed -r 's/&/\\&amp;/g')
-        fi
-        
-        sed -i -e "s/WF_SNP_VCF/$wfvcf/g" "${OUTXML}"
-        sed -i -e "s/PHASED_VCF/$phasevcf/g" "${OUTXML}"
-        sed -i -e "s/C1_LR_BAM_URL/$lrbam/g" "${OUTXML}"
-        sed -i -e "s/C1_LR_index_URL/$lrbai/g" "${OUTXML}"
-    done < "${BASEDIR}/${run}.info"
 }
 
 # Prepare input file
@@ -513,8 +391,6 @@ process_samples() {
                     "/data/${Barcode}/${Episode}.vcf"
             fi
 
-
-
             # Cleanup temporary files
             rm -rf "${WORKDIR}/${Barcode}/work"
             if [[ "$Variant1" =~ chr ]] && [[ "$Variant2" =~ chr ]]; then
@@ -524,23 +400,18 @@ process_samples() {
                 rm "${WORKDIR}/${Barcode}/clean-span-hq.bam"*
             fi
 
-        # Generate XML file for this sample
+            # Generate XML file for this sample
             log "Generating XML for ${Barcode}/${Episode}..."
             generate_xml_single "$RUNID" "$Barcode" "$Episode" "$Coordinate" "$Variant1" "$Variant2" "$EpisodeWES"
             
-        # prepare data for the xml file
+            # Upload data to S3
             aws s3 cp ${WORKDIR}/${Barcode} s3://nswhp-gaia-poc-pl/ONT/${RUNID}/${Barcode}/ --recursive >/dev/null 2>&1
-            
-                log "Upload finished!\n"
-        
-               
+            log "Upload finished!\n"
         }
     done < "${BASEDIR}/${RUNID}.info"
 }
 
-#===============================================================================
-# Main Script Execution
-#===============================================================================
+# Main execution
 
 # Prepare input file
 prepare_input_file
